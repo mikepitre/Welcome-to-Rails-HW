59700a621c3caaa8da2fbadaeb0df53a15b9921c
Mike Pitre <michaelepitre@gmail.com>
2015-06-02 14:55:37 -0500

    finished editing controller for normal mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
 
+  def index
+    all_users = User.all
+    string_user = all_users.map do |user|
+      "#{user.first_name} #{user.last_name}, #{user.age}"
+    end
+    render text: string_user.join(" | ")
+  end
 
-
-
-
-
-
-
-
-
-
+  def show
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+      render text: "#{user.first_name} #{user.last_name}, #{user.age}"
+    else
+      render text: "User Not Found", status: 404
+    end
+  end
 
 end
 
